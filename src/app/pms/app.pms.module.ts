7e249833818aa8e3294aa7f59a835aafca91c538
pahi7001 <hiral.parmar.consultant@nielsen.com>
2018-04-19 19:31:04 +0530

    build fixes

--- src/app/pms/app.pms.module.ts
+++ src/app/pms/app.pms.module.ts
@@ -9,16 +9,19 @@ import { SmMenuComponent } from './components/sm-menu/sm-menu.component';
 import { OwlMomentDateTimeModule } from 'ng-pick-datetime-moment';
 import { OwlDateTimeModule } from 'ng-pick-datetime';
 import { OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
-import { UserLookupComponent } from './components/user-lookup/user-lookup.component';
+// import { UserLookupComponent } from './components/user-lookup/user-lookup.component';
 import { SelectDropDownModule } from "ngx-select-dropdown";
-import { SearchComponent } from './components/search/search.component';
+// import { SearchComponent } from './components/search/search.component';
 import {AdminComponent} from './components/admin/admin.component';
+import {EmployeeComponent} from './components/employee/employee.component';
+import {ManagerComponent} from './components/manager/manager.component';
+import {FeedbackComponent} from './components/feedback/feedback.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PmsRoutingModule, CoreComponentsModule
   ],
-  declarations: [DashboardComponent, SideNavComponent, SmMenuComponent, AdminComponent]
+  declarations: [DashboardComponent, SideNavComponent, SmMenuComponent, AdminComponent, FeedbackComponent, ManagerComponent, EmployeeComponent]
 })
 export class PmsModule { }
