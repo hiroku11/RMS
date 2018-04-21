4efe510a7d9e8d0d8c854ae2c142e15b85b90586
hiroku11 <33553628+hiroku11@users.noreply.github.com>
2018-04-21 10:56:21 +0530

    few pms changes

--- src/app/pms/pms.routing.module.ts
+++ src/app/pms/pms.routing.module.ts
@@ -1,12 +1,18 @@
+import { ManagerComponent } from './components/manager/manager.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import {AdminComponent} from './components/admin/admin.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserService } from '../services/user.service';
 const pmsRoutes: Routes = [
-    { path: '', redirectTo:"dashboard"},
-    { path: 'dashboard', component: DashboardComponent},
-    { path: 'admin', component:AdminComponent}
+    { path: '', redirectTo: "dashboard" },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: 'feedback', component: FeedbackComponent },
+    { path: 'employee', component: EmployeeComponent },
+    { path: 'manager', component: ManagerComponent }
 ]
 
 @NgModule({
