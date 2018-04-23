73899f5b73574fd55e191f9666dd68832bc3fc65
hiroku11 <33553628+hiroku11@users.noreply.github.com>
2018-04-23 20:13:04 +0530

    cycle table

---
+++ src/app/pms/components/feedback/360-feedback/360-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-360-feedback',
+  templateUrl: './360-feedback.component.html',
+  styleUrls: ['./360-feedback.component.scss']
+})
+export class Three60FeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
