e2f28483e1276958f7e553abf6e663c329ffff27
pahi7001 <hiral.parmar.consultant@nielsen.com>
2018-05-01 18:43:51 +0530

    docs in progress

--- src/app/pms/components/document/document.component.ts
+++ src/app/pms/components/document/document.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AlertsLoaderService } from './../../../services/alerts-loader.service';
+import { ApiService } from './../../../services/api.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { UserService } from './../../../services/user.service';
 
 @Component({
   selector: 'app-document',
@@ -6,19 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document.component.scss']
 })
 export class DocumentComponent implements OnInit {
-
-  constructor() { }
+  @Input() userPerformanceCycleId: number;
+  data:any;
+  constructor(private route: ActivatedRoute, private _api: ApiService, private _alert: AlertsLoaderService, private userService: UserService) { }
 
   ngOnInit() {
+    this.getDocument()
   }
 
   documentSelected($event: any){
 
   }
-  downloadDocument(doc:any){
+  downloadDocument(id:number){
+    this._api.get(`/performance/download-performance-document/id/${id}`).subscribe(
+      (data)=>{
+        this.data = data;
+      },(error)=>{
 
+      }
+    )
   }
-  deleteDocumnet(doc:any, index: number){
+  deleteDocumnet(id:number){
+  
+    this._api.delete(`performance/delete-performance-document/userPerformanceCycleId/`+ this.userPerformanceCycleId +`/id/${id}`).subscribe(
+      (data)=>{
+        this.data = data;
+      },(error)=>{
 
+      }
+    )
+  }
+  getDocument(){
+    this._api.get(`/performance/performance-documents/userPerformanceCycleId/`+ this.userPerformanceCycleId).subscribe(
+      (data)=>{
+        this.data = data;
+      },(error)=>{
+
+      }
+    )
   }
 }
