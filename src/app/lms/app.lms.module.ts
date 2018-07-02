b5f78ca1a266410162ecba19df61ff0cce32f7c2
hiroku11 <33553628+hiroku11@users.noreply.github.com>
2018-07-03 01:53:11 +0530

    profiles and courses

--- src/app/lms/app.lms.module.ts
+++ src/app/lms/app.lms.module.ts
@@ -1,3 +1,10 @@
+import { ManageCourseListComponent } from './components/manage-courses/manage-course-list/manage-course-list.component';
+import { PaginationModule } from 'ngx-pagination-bootstrap';
+import { AddProfileComponent } from './components/manage-profiles/add-profile/add-profile.component';
+import { ProfilesListComponent } from './components/manage-profiles/profiles-list/profiles-list.component';
+import { ManageProfilesComponent } from './components/manage-profiles/manage-profiles.component';
+import { AddCourseComponent } from './components/manage-courses/add-course/add-course.component';
+import { ManageCoursesComponent } from './components/manage-courses/manage-courses.component';
 import { LmsRoutingModule } from './lms.routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CoreComponentsModule } from './../core.components.module/core.components.module';
@@ -7,16 +14,19 @@ import { NgModule } from '@angular/core';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { SmMenuComponent } from './components/sm-menu/sm-menu.component';
 import { CourseComponent } from './components/courses/course.component';
-import { CourseListComponent} from './components/courses/course-list/course-list.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
 import { MyCoursesComponent } from './components/mycourses/mycourses.component'
 import { ProgressComponent } from './components/progress/progress.component';
-import { CourseDetailComponent} from './components/courses/course-detail/course-detail.component';
-import { UserCourseDetailComponent} from './components/user-course-detail/user-course-detail.component';
+import { CourseDetailComponent } from './components/courses/course-detail/course-detail.component';
+import { UserCourseDetailComponent } from './components/user-course-detail/user-course-detail.component';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
 @NgModule({
     declarations: [DashboardComponent, SideNavComponent, SmMenuComponent,
-        CourseComponent,MyCoursesComponent,ProgressComponent,UserCourseDetailComponent,CourseDetailComponent,
-        CourseListComponent],
-    imports: [CommonModule, FormsModule, CoreComponentsModule, LmsRoutingModule],
+        CourseComponent, MyCoursesComponent, ProgressComponent, UserCourseDetailComponent,
+        CourseDetailComponent, CourseListComponent, ManageCoursesComponent, AddCourseComponent,
+        ManageProfilesComponent, ProfilesListComponent, AddProfileComponent, ManageCourseListComponent],
+    imports: [CommonModule, FormsModule, CoreComponentsModule, LmsRoutingModule,
+        SelectDropDownModule, PaginationModule],
     providers: []
 })
 export class LmsModule { }
--- src/app/lms/lms.routing.module.ts
+++ src/app/lms/lms.routing.module.ts
@@ -1,25 +1,47 @@
+import { AddCourseComponent } from './components/manage-courses/add-course/add-course.component';
+import { ManageCourseListComponent } from './components/manage-courses/manage-course-list/manage-course-list.component';
+import { AddProfileComponent } from './components/manage-profiles/add-profile/add-profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CourseComponent } from './components/courses/course.component';
 import { MyCoursesComponent } from './components/mycourses/mycourses.component';
-import { CourseListComponent} from './components/courses/course-list/course-list.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
 import { ProgressComponent } from './components/progress/progress.component';
 import { UserCourseDetailComponent } from './components/user-course-detail/user-course-detail.component';
-import { CourseDetailComponent} from './components/courses/course-detail/course-detail.component';
+import { CourseDetailComponent } from './components/courses/course-detail/course-detail.component';
+import { ProfilesListComponent } from './components/manage-profiles/profiles-list/profiles-list.component';
+import { ManageProfilesComponent } from './components/manage-profiles/manage-profiles.component';
 
 const lmsRoutes = [
     { path: '', redirectTo: "dashboard" },
-    { path: 'dashboard' ,component: DashboardComponent},
-    { path: 'courses', component: CourseComponent, children: [
-        { path: '', pathMatch: 'full', component: CourseListComponent },
-        { path: 'course-detail/:id', component: CourseDetailComponent, pathMatch: 'full'}
-    ]},
+    { path: 'dashboard', component: DashboardComponent },
+    {
+        path: 'courses', component: CourseComponent, children: [
+            { path: '', pathMatch: 'full', component: CourseListComponent },
+            { path: 'course-detail/:id', component: CourseDetailComponent, pathMatch: 'full' }
+        ]
+    },
     // { path: 'courses', component:CourseComponent},
-    { path: 'course-detail', component: CourseDetailComponent},
-    { path: 'my-course',component:MyCoursesComponent},
-    { path: 'progress', component : ProgressComponent},
-    {path: 'user-course-detail/:id', component:UserCourseDetailComponent}
+    { path: 'course-detail', component: CourseDetailComponent },
+    { path: 'my-courses', component: MyCoursesComponent },
+    { path: 'progress', component: ProgressComponent },
+    { path: 'user-course-detail/:id', component: UserCourseDetailComponent },
+    {
+        path: 'manage-profiles', component: ManageProfilesComponent, children: [
+            { path: '', pathMatch: 'full', component: ProfilesListComponent },
+            { path: 'add-profile', pathMatch: 'full', component: AddProfileComponent },
+            { path: 'edit-profile/:id', pathMatch: 'full', component: AddProfileComponent },
+        ]
+    },
+    {
+        path: 'manage-courses', component: ManageProfilesComponent, children: [
+            { path: '', pathMatch: 'full', component: ManageCourseListComponent },
+            { path: 'add-course', pathMatch: 'full', component: AddCourseComponent },
+            { path: 'edit-course/:id', pathMatch: 'full', component: AddCourseComponent },
+        ]
+    },
+
 ]
 @NgModule({
     imports: [
