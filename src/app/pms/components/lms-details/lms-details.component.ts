7ae2984b510230a5af33ec0f33488d2973ce9b6a
pahi7001 <hiral.parmar.consultant@nielsen.com>
2018-08-12 17:12:47 +0530

    LMS tan in pms done

--- src/app/pms/components/lms-details/lms-details.component.ts
+++ src/app/pms/components/lms-details/lms-details.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+
+import { HttpActionDirective } from '../../../core.components.module/directives/http-action.directive';
+import { AlertsLoaderService } from '../../../services/alerts-loader.service';
+import {
+  Component,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  Input
+} from '@angular/core';
+import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
 
 @Component({
   selector: 'app-lms-details',
@@ -7,9 +17,34 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LmsDetailsComponent implements OnInit {
   @Input() cycle: any;
-  constructor() { }
-
+  constructor(private _apiService: ApiService,
+    private _alertService: AlertsLoaderService,
+    private viewContainerRef: ViewContainerRef) { }
+    courseData =[];
+    itemsCount = 0;
+    searchParams: any = {
+      paging: { currentPage: 0, pageSize: 10 },
+      sorts: [
+        {
+          field: "description",
+          order: "ASC"
+        }
+      ],
+      filters: []
+    };
   ngOnInit() {
+    this.getCourse();
   }
-
+  getCourse(){
+    this._apiService
+    .get("/user-course/my-courses")
+    .subscribe(data => {
+      this.courseData = data.userCourses; 
+    });
+  }
+  getPageData($event: any) {
+    this.searchParams.paging.currentPage = $event.pageNo - 1;
+    this.searchParams.paging.pageSize = $event.pageSize;
+    this.getCourse();
+  }
 }
